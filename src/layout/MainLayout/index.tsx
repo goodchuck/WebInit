fdc6d87ad8c3381e386633434f34ab41dd7b1f32
goodchuck <y502100@naver.com>
2024-08-13 00:25:02 +0900

    Style : MainLayout ESLint 문제 해결

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -1,19 +1,25 @@
 import { Flex, Tag } from 'antd';
 import { StyledMainLayout } from './style';
 
-interface IMainLayout {
+interface MainLayoutProps {
   title: string;
-  tags?: string[];
+  tags: string[];
 
   children: React.ReactNode;
 }
 
-export function MainLayout({ title = '테스트', tags, children }: IMainLayout) {
+function MainLayout({
+  title = '테스트',
+  tags = [''],
+  children,
+}: MainLayoutProps) {
   return (
     <StyledMainLayout>
       <h1>{title}</h1>
-      <Flex>{tags?.map((tag, index) => <Tag key={index}>{tag}</Tag>)}</Flex>
+      <Flex>{tags?.map((tag) => <Tag key={Math.random()}>{tag}</Tag>)}</Flex>
       {children}
     </StyledMainLayout>
   );
 }
+
+export default MainLayout;
